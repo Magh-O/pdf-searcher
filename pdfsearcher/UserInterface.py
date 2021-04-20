21d867edaf76f4fc18af7a31c75a57fafce067f9
Adrián Moure Cerdeira <amc.3da@gmail.com>
2021-04-20 17:44:11 +0200

    Window User Interface Working

--- pdfsearcher/UserInterface.py
+++ pdfsearcher/UserInterface.py
@@ -5,6 +5,7 @@ import pathlib
 
 import tkinter as tk
 import tkinter.filedialog as tk_fd
+import tkinter.ttk as tk_ttk
 
 import findPages
 
@@ -18,25 +19,30 @@ class UserInterface(tk.Frame):
 
     def create_widgets(self):
         self.file_path_lbl = tk.Label(self, text="Selecciona un directorio de búsqueda", 
-                                    font=("Arial Bold", 20))
-        self.file_path_lbl.pack()
+                                    font=("Arial Bold", 10))
+        self.file_path_lbl.pack(fill=tk.X)
 
-        self.select_path_btn = tk.Button(self, text="Directorio",
+        self.select_path_btn = tk.Button(self, text="Seleccionar Directorio",
                                     command=self.select_file_path)
-        self.select_path_btn.pack()    
+        self.select_path_btn.pack(fill=tk.X)    
 
-        self.word_search_ent = tk.Entry(self, width=20)
-        self.word_search_ent.pack()
+        self.word_search_ent = tk.Entry(self)
+        self.word_search_ent.pack(fill=tk.X)
 
         self.search_btn = tk.Button(self, text="Buscar",
-                                    command=self.search_word)
-        self.search_btn.pack()
+                                    command=self.result_window)
+        self.search_btn.pack(fill=tk.X)
+
+        #self.progress_bar = tk_ttk.Progressbar(self, mode="indeterminate")
+        #self.progress_bar.pack()
 
     def select_file_path(self):
         path = tk_fd.askdirectory(title="Select a folder")
         self.file_path_lbl["text"] = path
 
     def search_word(self):
+        result_text = ""
+
         if self.file_path_lbl["text"]: 
             for subdir, dirs, files in os.walk(self.file_path_lbl["text"]):
 
@@ -46,9 +52,23 @@ class UserInterface(tk.Frame):
                     if filepath.endswith(".pdf"):
                         found_pages = findPages.find_in_pdf(filepath, 
                                                             self.word_search_ent.get())    
-                        print(self.file_path_lbl["text"])
-                        print(self.word_search_ent.get()) 
-                        print(found_pages)
+
+                        result_text += f'Directorio: {filename}\nPáginas encontradas: {found_pages}\n///////////////\n'
+
+            
+        return result_text
+    
+    def result_window(self):
+        popup = tk.Toplevel()        
+        
+        result_text = self.search_word()        
+
+        result_lbl = tk.Text(popup)        
+        result_lbl.insert("1.0", result_text)
+        result_lbl.config(state="disabled")
+        result_lbl.pack()
+
+        popup.mainloop()
 
 root = tk.Tk()
 ui = UserInterface(master=root)
