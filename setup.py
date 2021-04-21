2a313402944656af457d9feab01abdd8ba3ec1e3
Adrián Moure Cerdeira <amc.3da@gmail.com>
2021-04-21 10:26:38 +0200

    Updated the setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
 setup(
     name="pdf-reader",
     version="1.0.0",
